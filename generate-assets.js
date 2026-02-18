9e2d3e211e3f83c0a2f32dce2796b45e54b68e87
Web Developer | Hassan Biswas <hassanbiswas.github.io@gmail.com>
2026-02-18 11:35:31 +0600

    Update generate-assets.js

--- generate-assets.js
+++ generate-assets.js
@@ -17,7 +17,7 @@ const CONFIG = {
 
 const EXCLUSIONS = {
   folders: ['dist', 'node_modules', '.git', '.github', 'assets', 'components', 'draft', 'error', 'freefire', 'nid', 'trash'],
-  files: ['README.md', 'generate-assets.js', 'googledb2beed158567cde.html', 'heartbeat.json', 'lighthouserc.json', 'manifest.webmanifest', 'package.json', 'sw.js', '404.html', 'favicon.ico', 'favicon.svg', 'offline.html', '8886230f8ec14aecb0ba333e57bc135a.txt']
+  files: ['README.md', 'generate-assets.js', 'googledb2beed158567cde.html', 'LICENSE.txt', 'heartbeat.json', 'lighthouserc.json', 'manifest.webmanifest', 'package.json', 'sw.js', '404.html', 'favicon.ico', 'favicon.svg', 'offline.html', '8886230f8ec14aecb0ba333e57bc135a.txt']
 };
 
 const getHtmlFiles = (dir, fileList = []) => {
