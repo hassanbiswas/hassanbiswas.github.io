4a890ecd48c1269c972b3d8b0107d08303577788
hassanbiswas <hassanbiswas.github.io@gmail.com>
2026-03-15 03:20:48 +0000

    chore: automated version bump to 26.03.15 [skip ci]

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const now = new Date();
 const YY = now.getFullYear().toString().slice(-2);
 const MM = (now.getMonth() + 1).toString().padStart(2, '0');
 const DD = now.getDate().toString().padStart(2, '0');
-const VERSION = `${YY}.${MM}.${DD}`;
+const VERSION = '26.03.15';
 
 const CACHE_NAME = `hassan-biswas-v${VERSION}`;
 const OFFLINE_URL = '/index.html';
