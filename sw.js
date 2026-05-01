6966eccadb0f17979568c82dabf719987bdb39af
hassanbiswas <hassanbiswas.github.io@gmail.com>
2026-05-01 04:02:55 +0000

    Release - Stable Build [26.05.01] [skip ci]

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Automated Dynamic Versioning & Cache-Busting Logic (YY.MM.DD)
  */
 
-const VERSION = new Date().toLocaleDateString('en-GB').split('/').reverse().join('.');
+const VERSION = '26.05.01';
 const CACHE_NAME = `hassan-biswas-v${VERSION}`;
 const OFFLINE_URL = '/index.html';
 
